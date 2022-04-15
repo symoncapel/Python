faacc12f13ec5925f099c1dc7eda8dcc5c4a3a88
Symon <78396721+symoncapel@users.noreply.github.com>
2022-04-15 00:27:21 -0600

    first commit

---
+++ How_to_Sort_Lists_in_Python.py
@@ -0,0 +1,17 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+print(tags)
+
+tags.sort()
+
+print(tags)
+
+tags.sort(reverse=True)
+
+print(tags)
+
+totals = [234, 1, 543, 2345]
+
+totals.sort(reverse=True)
+
+print(totals)
