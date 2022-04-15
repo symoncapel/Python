8ca54731260d096056b9724725af9cd72c0e9180
Symon <78396721+symoncapel@users.noreply.github.com>
2022-04-15 00:39:26 -0600

    Update How_to_Sort_Lists_in_Python.py

--- How_to_Sort_Lists_in_Python.py
+++ How_to_Sort_Lists_in_Python.py
@@ -1,17 +1,16 @@
 tags = ['python', 'development', 'tutorials', 'code']
-
 print(tags)
 
 tags.sort()
-
 print(tags)
 
 tags.sort(reverse=True)
-
 print(tags)
 
 totals = [234, 1, 543, 2345]
+totals.sort()
+print(totals)
 
+totals = [234, 1, 543, 2345]
 totals.sort(reverse=True)
-
 print(totals)
