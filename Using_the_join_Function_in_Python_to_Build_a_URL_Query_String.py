a61200911a8aaa17710310708a49c1299c6a6c77
Symon <78396721+symoncapel@users.noreply.github.com>
2022-04-15 13:24:06 -0600

    commit

---
+++ Using_the_join_Function_in_Python_to_Build_a_URL_Query_String.py
@@ -0,0 +1,7 @@
+# https://www.google.com/search?q=python+development+tutorial
+
+uri = 'https://www.google.com/search?q='
+tags = ['python', 'development', 'tutorial']
+formatted_tags = '+'.join(tags)
+
+print(formatted_tags)
