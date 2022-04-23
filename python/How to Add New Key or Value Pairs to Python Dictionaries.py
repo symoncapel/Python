6d19cc1e991ecb4d51dc0e1b6c2491fd55c4e313
Symon <78396721+symoncapel@users.noreply.github.com>
2022-04-23 00:12:25 -0600

    yuk

--- python/How to Add New Key or Value Pairs to Python Dictionaries.py
+++
@@ -1,9 +0,0 @@
-teams = {
-  "astros": ["Altuve", "Correa", "Bregman"],
-  "angels": ["Trout", "Pujols"],
-  "yankees": ["Judge", "Stanton"],
-}
-      #adding a new key dictionary
-teams['red sox'] = ['Price', 'Betts']
-
-print(teams)
--- python/Using_the_join_Function_in_Python_to_Build_a_URL_Query_String.py
+++
@@ -1,8 +0,0 @@
-# https://www.google.com/search?q=python+development+tutorial
-
-uri = 'https://www.google.com/search?q='
-tags = ['python', 'development', 'tutorial']
-formatted_tags = '+'.join(tags)
-query_uri = f'{uri}{formatted_tags}'
-
-print(query_uri)
