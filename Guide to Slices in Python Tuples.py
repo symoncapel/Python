4d2d6684ef9ca365bf772158646ba6bc7c2da575
Symon Capel <symon.capel@gmail.com>
2022-04-23 11:16:11 -0600

    first commit

---
+++ Guide to Slices in Python Tuples.py
@@ -0,0 +1,3 @@
+post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
+
+print(post[1::2])
---
+++ Working with Lists Nested in Tuples.py
@@ -0,0 +1,7 @@
+post = ('Python Basics', 'Intro guide to Python', 'Some cool python content')
+
+tags = ['python', 'coding', 'tutorial']
+
+post += (tags,)
+
+print(post[-1][1])
