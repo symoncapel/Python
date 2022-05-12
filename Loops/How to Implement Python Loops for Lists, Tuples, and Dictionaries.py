ee1aee13bb6e95bb953ccc74a2990a5f4b7ba6bb
symoncapel~ <symon.capel@gmail.com>
2022-05-12 13:05:21 -0600

    commit

--- Loops/How to Implement Python Loops for Lists, Tuples, and Dictionaries.py
+++ Loops/How to Implement Python Loops for Lists, Tuples, and Dictionaries.py
@@ -1,4 +1,25 @@
-players = ['Altuve', 'Bregman', 'Correa', 'Gattis']
+#players = ['Altuve', 'Bregman', 'Correa', 'Gattis']
 
-for player in players:
-    print(player)
+#for player in players:
+#    print(player)
+
+
+#players = {
+#    '2b': 'Altuve',
+#    '3b': 'Bregman',
+#    'ss': 'Correa',
+#    'dh': 'Gattis',
+#}
+
+#for position, player in players.items():
+#    print('Position', position)
+#    print('Player Name', player)
+
+
+my_list = ['yes', 'no', 'maybe', 'absolutly', 'bless your heart']
+def loop_over_list():
+    for item in my_list:
+        print(item)
+    return my_list
+
+loop_over_list()
---
+++ Loops/How to Loop Through the Characters of a Python String.py
@@ -0,0 +1,13 @@
+#alphabet = 'abcdef'
+
+#for letter in alphabet:
+#  print(letter)
+
+
+name = 'Symon'
+def loop_over_string():
+    for letters in name:
+        print(letters)
+    return name
+    
+loop_over_string()
