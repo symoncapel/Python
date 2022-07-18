d76ec169283b883c09dd64f571b33b6ca30afae6
symoncapel~ <symon.capel@gmail.com>
2022-07-17 18:01:20 -0600

    commit

--- Classes/How to Work with Python Properties and Decorators.py
+++ Classes/How to Work with Python Properties and Decorators.py
@@ -33,10 +33,12 @@ We want to ensure that our size attribute can be retrieved, but not set. Use the
 class Garage:
   def __init__(self, size):
     #   Protect the size attribute
-    self.size = size
+    self._size = size
     self.cars = []
 
-  # add decorator here
+  @property
+  def size(self):
+    return self._size
 
   def open_door(self):
     return "The door opens"
